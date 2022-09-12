cf6ee78cc8d4006c79c7761dbe29d4725e746e8b
Caro Marin <developer.cm27@gmail.com>
2022-09-12 11:10:03 -0300

    primer commit

---
+++ controllers/testimonialController.js
@@ -0,0 +1,54 @@
+import { Testimonial } from '../models/Testimoniales.js';
+
+const guardarTestimonial = async (req, res) => {
+  // Validar...
+
+  const { nombre, email, mensaje } = req.body;
+  const errores = [];
+
+  if(nombre.trim() === '') {
+    errores.push({mensaje: 'El nombre esta vacio'})
+  }
+
+  if(email.trim() === '') {
+    errores.push({mensaje: 'El email esta vacio'})
+  }
+
+  if(mensaje.trim() === '') {
+    errores.push({mensaje: 'El nombre esta vacio'})
+  }
+
+  if(errores.length > 0) {
+
+    //Consultar testimoniales existentes
+    const testimoniales = await Testimonial.findAll();
+    
+    // Mostrar la vista con errores
+    res.render('testimoniales', {
+      pagina: 'Testimoniales',
+      errores,
+      nombre,
+      email,
+      mensaje,
+      testimoniales
+    })
+  } else {
+    // Almacenarlo en la base de datos
+
+    try {
+      await Testimonial.create({
+        nombre,
+        email,
+        mensaje
+      });
+
+      res.redirect('/testimoniales');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export {
+  guardarTestimonial
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+  .then( () => console.log('Base de datos conectada'))
+  .catch(error => console.log(error));
+
+const port = process.env.PORT || 4000;
+
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Obtener el year actual
+app.use( (req, res, next) => {
+  const year = new Date();
+  res.locals.currentYear = year.getFullYear();
+  res.locals.siteName = 'Agencia de Viajes'
+  next();
+})
+
+// agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar router
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
